f928ce8e5624bf05225bf3d7f8a764dd043ed547
Ghina <h1101241015@student.untan.ac.id>
2026-05-09 11:08:42 +0700

    Setup RBAC practicum project

--- src/infrastructure/database/prisma.client.ts
+++ src/infrastructure/database/prisma.client.ts
@@ -1,17 +1,4 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
-import mysql from 'mysql2/promise';
-
-
-const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
-});
 
 export const prisma = new PrismaClient();
-
-async function test() {
-  const users = await prisma.user.findMany();
-  console.log(users);
-}
-
-test();
